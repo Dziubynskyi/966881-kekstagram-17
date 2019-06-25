3151d018781999e731d6441383077237dd10c4e5
Yuri Dziubynskyi <dziubynskyi@gmail.com>
2019-06-25 20:52:34 +0300

    module4-task2

--- js/upload.js
+++ js/upload.js
@@ -5,37 +5,25 @@ var popupOpen = document.querySelector('#upload-file');
 var popupClose = document.querySelector('#upload-cancel');
 var textDescription = document.querySelector('.text__description');
 
-textDescription.addEventListener('keyup', function(evt) {
-if (evt.target===textDescription){
-document.removeEventListener('keydown', onPopupEscPress);
-}
-else {
-  textDescription.removeEventListener('keyup');
-}
-});
-
 popupOpen.addEventListener('change', function() {
-editImageForm.classList.remove('hidden');
+  //а вот тут-то мы и должны навешивать наш обработчик onPopupEscapePress
+  document.addEventListener("keydown", onPopupEscPress)
+  editImageForm.classList.remove('hidden');
 });
 
 popupClose.addEventListener('click', function() {
- closePopup();
+  closePopup();
 });
 
-popupOpen.addEventListener('keydown', function(evt) {
-  if (evt.keyCode === ESC_KEYCODE) {
-    closePopup();
-  }});
-
 function onPopupEscPress(evt) {
-  if (evt.keyCode === ESC_KEYCODE) {
+  //вот именно здесь мы проверяем, на каком элементе изначально случилось событие
+  if (evt.keyCode === ESC_KEYCODE && evt.target !== textDescription) {
     closePopup();
-  }};
+  }
+};
 
 function closePopup() {
   editImageForm.classList.add('hidden');
   document.removeEventListener('keydown', onPopupEscPress);
-  popupOpen.value=null;
+  popupOpen.value = null;
 };
-
-
